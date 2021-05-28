33f5696850c9dd13d9d67aab34506b242384f376
Loïc Salanon <loic.salanon@ac-lyon.fr>
2021-05-28 11:20:35 +0200

    [v-gouv-fr-accordion] Ajout composant Accordion

---
+++ v-gouv-fr-accordion/src/VGouvFrAccordionsGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="v-gouv-fr-accordions-group fr-accordions-group">
+    <slot></slot>
+  </ul>
+</template>
+<script>
+import "@gouvfr/dsfr/dist/css/dsfr.min.css";
+import "@gouvfr/dsfr/dist/js/dsfr.nomodule.min.js";
+  export default {
+    name: 'v-gouv-fr-accordions-group',
+    props: {
+    },
+    data: () => ({
+    }),
+    methods: {
+    }
+  }
+</script>
